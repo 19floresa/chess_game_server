9bb619fb1b03dfb74e50db7b61099b41716b2456
19floresa <alexander.flores0920@gmail.com>
2025-12-26 13:04:24 -0800

    added error checking and better output for response message

--- src/controllers/gameControllers.ts
+++ src/controllers/gameControllers.ts
@@ -9,23 +9,35 @@ export default function gameSearch(req: Request, res: Response): void
     {
         const { id }: { id: string } = req.body
         const playerId: number = parseInt(id)
-        const port: number = getPort()
         const gameId: number = games.findMatch(playerId) // TODO: Check players are not already in a match
         if (gameId === -2)
         {
-            throw new Error("Light and dark player can not be the same person!")
+            throw new Error("Player 1 and 2 can not be the same person!")
         }
-        else if (gameId === -1)
+        
+        const respMsg = { message: "", gameId, port: getPort() } // TODO: remove sending port
+        if (gameId === -1)
         { 
             // Create a new game
-            games.changeState({ newState: state.searching, userId: playerId, gameId: -1 })
+            respMsg.message = "New game was created! Waiting on player 2."
+            const result: boolean = games.changeState({ newState: state.searching, userId: playerId, gameId: -1 })
+            if (result === false)
+            {
+                // Should never be reached
+                throw new Error("New game could not be created.")
+            }
         }
         else
         {
             // Add player 2 to a game already created
-            games.changeState({ newState: state.active, userId: playerId, gameId })
+            respMsg.message = "Game was found!"
+            const result: boolean = games.changeState({ newState: state.active, userId: playerId, gameId })
+            if (result === false)
+            {
+                throw new Error("Game was not found.")
+            }
         }
-        res.send({ message: "Searching for player...", port }) // TODO: maybe remove sending port
+        res.send(respMsg)
     }
     catch (e)
     {
