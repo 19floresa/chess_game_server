2fd5d14de1f5cbaa4ddddb899765325ad7fb87a2
19floresa <alexander.flores0920@gmail.com>
2025-12-26 14:44:07 -0800

    added a new state and socket connected in gameState

--- lib/types/state.ts
+++ lib/types/state.ts
@@ -1,12 +1,15 @@
 export enum state {
-    searching,
+    initialize,
+    ready,
     active,
     complete
 }
 
 export interface gameState {
-    userIdLight: number
-    userIdDark: number
+    idP1: number // light
+    idP2: number // dark
+    connectedP1: boolean
+    connectedP2: boolean
     gameId: number
     gameHistory: []
     timeStarted: number
