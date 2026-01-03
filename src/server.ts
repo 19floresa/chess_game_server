a9cd451e7c17413c67584b93ebf74a6a74c8de42
19floresa <alexander.flores0920@gmail.com>
2026-01-03 00:22:55 -0800

    moved stuff to node dev dependency

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import httpServer from "./socket.ts"
 import config from "./config/config.ts"
 
-
 httpServer.listen(config.port, () => console.log(`Server listening on port ${config.port}`))
 
 // import color from "../lib/types/color.ts"
