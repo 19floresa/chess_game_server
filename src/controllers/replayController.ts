5d7070636a647e8341c1599719dd1f7c16d188ac
19floresa <alexander.flores0920@gmail.com>
2026-02-22 12:58:01 -0800

    b

--- src/controllers/replayController.ts
+++ src/controllers/replayController.ts
@@ -6,15 +6,16 @@ export default async function replayGame(req: Request, res: Response): Promise<v
     try
     {
         const { playerId, gameId } = req.body
-        const [ status, body ] = await postFetch({ playerId, gameId }, "replay")
-        if (status === 200)
-        {
-            res.send(body)
-        }
-        else
-        {
-            throw new Error("Replays could not be retrieved.")
-        }
+        console.log(playerId)
+        // const [ status, body ] = await postFetch({ playerId, gameId }, "replay")
+        // if (status === 200)
+        // {
+        //     res.send(body)
+        // }
+        // else
+        // {
+        //     throw new Error("Replays could not be retrieved.")
+        // }
     }
     catch (e)
     {
