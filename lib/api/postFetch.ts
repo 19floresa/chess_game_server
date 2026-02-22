15a0bf12a68a21791cc8bd9a08a312caf4b20032
19floresa <alexander.flores0920@gmail.com>
2026-02-22 12:53:08 -0800

    testing if chaning url will hep

--- lib/api/postFetch.ts
+++ lib/api/postFetch.ts
@@ -1,6 +1,6 @@
 export default async function postFetch(data: any, cmd: string): Promise< [ number, any ] >
 {
-    const res = await fetch(`http://127.0.0.1:3078/database/${cmd}`, 
+    const res = await fetch(`http://db:3078/database/${cmd}`, 
     { 
         method: 'POST', 
         headers: { "Content-Type": "application/json" },
